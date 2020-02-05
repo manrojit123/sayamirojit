f06491f206f0581516e4c772a6a123033519a615
rojit <sayami.rojit@gmail.com>
2020-02-05 08:35:00 +0545

    new site uploaded

---
+++ js/main.js
@@ -0,0 +1,71 @@
+(function ($) {
+  "use strict";
+  // TOP Menu Sticky
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 400) {
+      $("#sticky-header").removeClass("sticky");
+      $('#back-top').fadeIn(500);
+    } else {
+      $("#sticky-header").addClass("sticky");
+      $('#back-top').fadeIn(500);
+    }
+  });
+
+
+
+
+
+  $(document).ready(function () {
+
+    // mobile_menu
+    var menu = $('ul#navigation');
+    if (menu.length) {
+      menu.slicknav({
+        prependTo: ".mobile_menu",
+        closedSymbol: '+',
+        openedSymbol: '-'
+      });
+    };
+
+
+
+
+
+
+
+
+    // filter items on button click
+    $('.portfolio-menu').on('click', 'button', function () {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+    });
+
+    //for menu active class
+    $('.portfolio-menu button').on('click', function (event) {
+      $(this).siblings('.active').removeClass('active');
+      $(this).addClass('active');
+      event.preventDefault();
+    });
+
+
+
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+})(jQuery);	
