3e8f8174b0436c509819af8161fe310a04ec399a
rojit <sayami.rojit@gmail.com>
2020-03-24 10:20:40 +0545

    chagne lockdown

--- psl/basic/lib/const.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const minCOlor = Color(0xFF6AA452);
-  static const lightMainColor = Color(0xFF8676A);
-}
--- psl/basic/lib/main.dart
+++ psl/basic/lib/main.dart
@@ -1,26 +1,15 @@
+// import 'home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp(
-      TextInput: Text("Hello from main"),
-    ));
-
-class MyApp extends StatelessWidget {
-  final Widget TextInput;
-  MyApp(this.TextInput);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Hello",
-      home: new Scaffold(
-        appBar: AppBar(
-          title: Text('hello'),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[TextInput],
-          ),
-        ),
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
       ),
-    );
-  }
+    ),
+    home: Scaffold(
+      appBar: AppBar(title: Text("hello")),
+    ),
+  );
 }
