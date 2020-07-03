46e3361ad01cf39154b86146b01639fc29f35cd2
rojit <sayami.rojit@gmail.com>
2020-07-03 21:08:00 +0545

    demo sobey updated

---
+++ sobeyDemo/assets/js/sobey.js
@@ -0,0 +1,95 @@
+(function ($) {
+  "use strict";
+
+
+  if ($(window).width() < 768) {
+    // alert('Less than 960');
+  }
+  else {
+
+    function toggleDropdown(e) {
+      const _d = $(e.target).closest('.dropdown'),
+        _m = $('.dropdown-menu', _d);
+      setTimeout(function () {
+        const shouldOpen = e.type !== 'click' && _d.is(':hover');
+        _m.toggleClass('show', shouldOpen);
+        _d.toggleClass('show', shouldOpen);
+        $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+      }, e.type === 'mouseleave' ? 300 : 0);
+    }
+
+    $('body')
+      .on('mouseenter mouseleave', '.dropdown', toggleDropdown)
+      .on('click', '.dropdown-menu a', toggleDropdown);
+    // $('.dropdown a').on('click tap', e => e.preventDefault());
+  }
+
+
+  gsap.to("#scrollDown", {
+    y: 5,
+    repeat: -1,
+    duration: 1,
+    yoyo: true
+  });
+
+  gsap.to("#scrollDown .arrow", {
+    y: 20,
+    repeat: -1,
+    duration: 1,
+    yoyo: true
+  });
+  var msgs = ["Having issues with Connectivity ", "Lower your TCO with CBRS", "Flexible Capex and Opex options"];
+
+  var indx = 0;
+  $("#div").text(msgs[indx]);
+  setInterval(function () {
+    $("#div").fadeOut(
+      500,
+      function () {
+        indx++;
+        if (indx >= msgs.length) indx = 0;
+        $(this).text(msgs[indx]);
+        $(this).fadeIn(300);
+      });
+  }, 3000);
+
+
+
+  var msgs2 = ["Solve with CBRS", "Lower your TCO with CBRS", "Flexible Capex and Opex options"];
+
+  var indx2 = 0;
+  $("#div2").text(msgs2[indx]);
+  setInterval(function () {
+    $("#div2").fadeOut(
+      500,
+      function () {
+        indx2++;
+        if (indx2 >= msgs2.length) indx2 = 0;
+        $(this).text(msgs2[indx2]);
+        $(this).fadeIn(350);
+      });
+  }, 3100);
+
+  var $videoSrc;
+  $('.video-btn').click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+
+
+
+  // when the modal is opened autoplay it
+  $('#videoModal').on('shown.bs.modal', function (e) {
+
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+  })
+
+
+
+  // stop playing the youtube video when I close the modal
+  $('#videoModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src', $videoSrc);
+  });
+}(jQuery));
