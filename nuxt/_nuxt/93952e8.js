c42bf29fe659379469d5ffb0fb36082a4a7ba4b7
Rojit manandhar <sayami.rojit@gmail.com>
2020-09-23 15:03:32 +0545

    nuxt added

---
+++ nuxt/_nuxt/93952e8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{192:function(e,t,n){"use strict";n.r(t);var l={},c=n(21),component=Object(c.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("team Page")])}),[],!1,null,null,null);t.default=component.exports}}]);
